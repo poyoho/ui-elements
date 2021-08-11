ce15e37d8965f34b83509102cf74e25e4707f839
poyoho <907415276@qq.com>
2021-08-11 15:31:34 +0800

    feat(component): :triangular_flag_on_post: code-comment done

--- packages/code-comment/src/code-comment-element.ts
+++ packages/code-comment/src/code-comment-element.ts
@@ -7,6 +7,7 @@ export default html`
   <div class="source">
     <slot name="source"></slot>
   </div>
+  <div class="split"></div>
   <div class="comment">
     <slot name="comment" class="comment-text"></slot>
   </div>
@@ -16,14 +17,32 @@ export default html`
   display: flex;
   width: 100%;
 }
+.split {
+  width: 5px;
+  background: #000;
+  position: relative;
+  user-select: none;
+}
+.split::before {
+  content: "";
+  position: absolute;
+  display: block;
+  cursor: col-resize;
+  top: 0;
+  left: -5px;
+  width: 15px;
+  height: 100%;
+}
 .source {
-  flex: 1;
-  background: aqua;
+  width: 50%;
+  background: #282c34;
+  color: #d7d3cb;
   overflow: auto;
 }
 .comment {
-  flex: 1;
-  background: green;
+  width: 50%;
+  background: #282c3d;
+  color: #d7d3c2;
   overflow: auto;
 }
 </style>
