fb7dddded8ed5884521143d559037cc1b7328496
poyoho <907415276@qq.com>
2021-08-23 23:13:33 +0800

    build(component): :hammer: build all components

---
+++ scripts/plugins/rollup.raw.js
@@ -0,0 +1,33 @@
+const { parse: parseUrl, URLSearchParams } = require("url")
+const fs = require("fs")
+
+function parseRequest(id) {
+  const { search, href } = parseUrl(id)
+  if (!search) {
+    return null
+  }
+  return {
+    path: id.split("?")[0],
+    search: Object.fromEntries(new URLSearchParams(search.slice(1))),
+  }
+}
+
+function cleanUrl (url) {
+  const queryRE = /\?.*$/s
+  const hashRE = /#.*$/s
+  return url.replace(hashRE, '').replace(queryRE, '')
+}
+
+module.exports = function rollupWebWorker () {
+  return {
+    name: "raw-loader",
+    async load (id) {
+      const query = parseRequest(id)
+      if (query && query.search.raw !== null) {
+        return `export default ${JSON.stringify(
+          fs.readFileSync(query.path, { encoding: 'utf-8' })
+        )}`
+      }
+    },
+  }
+}
