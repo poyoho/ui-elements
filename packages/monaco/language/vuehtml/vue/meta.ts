dc63da8386c3cd206de88fa1b3098bc132a46a8f
poyoho <907415276@qq.com>
2021-09-04 19:00:54 +0800

    feat: monaco vuehtml add vfor

--- packages/monaco/language/vuehtml/vue/meta.ts
+++ packages/monaco/language/vuehtml/vue/meta.ts
@@ -1,23 +1,11 @@
 import { CompletionItemKind, CompletionItem } from 'vscode-html-languageservice'
 
-export const EventModifiers = {
-  stop: 'call event.stopPropagation().',
-  prevent: 'call event.preventDefault().',
-  capture: 'add event listener in capture mode.',
-  self: 'only trigger handler if event was dispatched from this element.',
-  // {keyAlias}: 'only trigger handler on certain keys.',
-  once: 'trigger handler at most once.',
-  left: 'only trigger handler for left button mouse events.',
-  right: 'only trigger handler for right button mouse events.',
-  middle: 'only trigger handler for middle button mouse events.',
-  passive: 'attaches a DOM event with { passive: true }.',
-}
-
-export const CustomTags = [
-  'slot',
-]
-
 export const Directives: CompletionItem[] = [
+  {
+    label: 'v-for',
+    insertText: 'v-for=""',
+    kind: CompletionItemKind.Function,
+  },
   {
     label: 'v-if',
     insertText: 'v-if=""',
