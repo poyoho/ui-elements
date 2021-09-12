cdfade49588a1c5f89020efa77fda1ebabb1bff4
poyoho <907415276@qq.com>
2021-09-12 21:52:46 +0800

    feat: icon of add button

--- packages/code-playground/src/code-playground-element.ts
+++ packages/code-playground/src/code-playground-element.ts
@@ -6,7 +6,8 @@ export default html`
 <drag-wrap direction="row">
   <div slot="item">
     <div id="tab">
-      <button>add</button>
+
+      <svg t="1631454598719" class="icon-add" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2150" width="14" height="14"><path d="M512 170.666667a42.666667 42.666667 0 0 1 42.666667 42.666666v256h256a42.666667 42.666667 0 1 1 0 85.333334h-256v256a42.666667 42.666667 0 1 1-85.333334 0v-256H213.333333a42.666667 42.666667 0 1 1 0-85.333334h256V213.333333a42.666667 42.666667 0 0 1 42.666667-42.666666z" fill="#e1e1e1" p-id="2151"></path></svg>
     </div>
     <drag-wrap direction="column" id="editor-wrap">
     </drag-wrap>
@@ -50,6 +51,9 @@ export default html`
 #tab button[active] {
   color: #e1e1e1;
   background: #333;
+}
+#tab .icon-add {
+  transform: translate(4px, 3px)
 }
 </style>
 `
