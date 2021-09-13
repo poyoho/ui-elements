722c1ad7cae61761192e61810cfee6d5340a1251
poyoho <907415276@qq.com>
2021-09-13 20:03:46 +0800

    feat(component): :sparkles: add and remove file tab

--- packages/code-playground/src/code-playground-element.ts
+++ packages/code-playground/src/code-playground-element.ts
@@ -7,9 +7,12 @@ export default html`
   <div slot="item">
     <div id="tab">
 
+      <input type="text" id="filename-input" />
       <svg t="1631454598719" class="icon-add" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2150" width="14" height="14"><path d="M512 170.666667a42.666667 42.666667 0 0 1 42.666667 42.666666v256h256a42.666667 42.666667 0 1 1 0 85.333334h-256v256a42.666667 42.666667 0 1 1-85.333334 0v-256H213.333333a42.666667 42.666667 0 1 1 0-85.333334h256V213.333333a42.666667 42.666667 0 0 1 42.666667-42.666666z" fill="#e1e1e1" p-id="2151"></path></svg>
     </div>
     <drag-wrap direction="column" id="editor-wrap">
+      <div slot="item" id="spacehold" hidden></div>
+      <slot name="editor"></slot>
     </drag-wrap>
   </div>
 
@@ -18,42 +21,62 @@ export default html`
   </div>
 </drag-wrap>
 <style>
-.sandbox {
-  height: 100%;
-  background: #333;
-}
-#vuehtml, #vuets {
-  width: 100%;
-  height: 100%;
-}
-.tablist {
-  font-size: 0;
-  cursor: pointer;
-}
-.tablist button {
-  font-size: 14px;
-  padding: 7px;
-}
-#tab {
-  background: #1e1e1e;
-}
-#tab button {
-  border: 0;
-  outline: 0;
-  background: #666;
-  color: #e1e1e1;
-  padding: 5px;
-  padding-right: 8px;
-}
-#tab button .icon {
-  transform: translate(4px, 3px)
-}
-#tab button[active] {
-  color: #e1e1e1;
-  background: #333;
-}
-#tab .icon-add {
-  transform: translate(4px, 3px)
-}
+  @keyframes input-error {
+    to {
+      transform: scale(.92);
+    }
+  }
+  .filename-input-error {
+    animation: input-error .1s linear 4 alternate;
+  }
+  .filename-input-show {
+    margin-left: 5px;
+    width: 80px !important;
+    opacity: 1 !important;
+    transition: width .1s;
+  }
+  #filename-input {
+    opacity: 0;
+    width: 0px;
+    color: #e1e1e1;
+    background: #1e1e1e;
+    outline: 0;
+    border: 1px solid #333;
+    transition: width .1s;
+  }
+  .sandbox {
+    height: 100%;
+    background: #333;
+  }
+  #tab button {
+    font-size: 13px;
+    border: 0;
+    outline: 0;
+    border-radius: 0;
+    background: #666;
+    color: #e1e1e1;
+    padding: 5px;
+    padding-right: 8px;
+  }
+  #tab button[closeable] {
+    padding-right: 20px;
+  }
+  #spacehold {
+    background: #1e1e1e;
+  }
+  #tab {
+    background: #1e1e1e;
+  }
+  #tab button .icon-close {
+    position: absolute;
+    transform: translate(3px, 1px);
+  }
+  #tab button[active] {
+    color: #e1e1e1;
+    background: #333;
+  }
+  #tab .icon-add {
+    transform: translateY(3px)
+  }
 </style>
 `
--- packages/code-playground/src/code-playground.ts
+++ packages/code-playground/src/code-playground.ts
@@ -13,6 +13,73 @@ interface MonacoEditorItem {
 
 type SupportEditorType = "vuehtml" | "ts"
 
+function createMonacoEditorManager (host: CodePlayground) {
+  const { editorWrap } = host
+  const editors: Record<string, MonacoEditorItem> = {}
+  const manager = {
+    show: (type: SupportEditorType): MonacoEditorItem => {
+      let state = editors[type]
+      if (!state) {
+        const editor = new MonacoEditor()
+        editor.style.width = "100%"
+        editor.style.height = "100%"
+
+        const wrap = host.ownerDocument.createElement("div")
+        wrap.style.width = "100%"
+        wrap.style.height = "100%"
+        wrap.setAttribute("slot", "item")
+        wrap.setAttribute("type", type)
+        wrap.appendChild(editor)
+
+        state = { wrap, editor, status: false }
+        editors[type] = state
+        editorWrap.appendChild(state.wrap)
+      }
+      if (!state.status) {
+        state.status = true
+        state.wrap.style.display = "block"
+        state.wrap.removeAttribute("hidden")
+      }
+      return state
+    },
+
+    getActive: () => {
+      const result = []
+      for (let key in editors) {
+        const editor = editors[key]
+        if (editor.status) {
+          result.push(editor)
+        }
+      }
+      return result
+    },
+
+    hide: (type: SupportEditorType) => {
+      const state = editors[type]
+      if (state && state.status) {
+        state.status = false
+        state.wrap.style.display = "none"
+        state.wrap.setAttribute("hidden", "")
+      }
+    },
+
+    hideAll: () => {
+      editorWrap.items.forEach(item => {
+        const type = item.getAttribute("type")
+        if (type) {
+          manager.hide(type as any)
+        }
+      })
+    },
+  }
+  return manager
+}
+
+function isCreateAble (filename: string, fs: FileSystem<CompiledFile>) {
+  return [".vue", ".ts"].some(extend => filename.endsWith(extend))
+    && !fs.isExist(filename)
+}
+
 async function createFile (host: CodePlayground,  filename: string, keepalive?: boolean) {
   const { editorManage, tabWrap, fs, editorWrap, sandbox } = host
   const project = await host.project
@@ -44,6 +111,7 @@ async function createFile (host: CodePlayground,  filename: string, keepalive?: 
     return model
   }
 
+  // TODO add dts
   async function activeMonacoEditor () {
     const isNotExistFile = !fs.isExist(filename)
 
@@ -112,23 +180,26 @@ async function createFile (host: CodePlayground,  filename: string, keepalive?: 
 
   function insertFileTab () {
     const filetab = document.createElement("button")
+    !keepalive && filetab.setAttribute("closeable", "")
     filetab.innerHTML = filename
-      + (keepalive ? '' : `<svg t="1631378872341" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1198" width="14" height="14"><path d="M466.773333 512l-254.72 254.72 45.226667 45.226667L512 557.226667l254.72 254.72 45.226667-45.226667L557.226667 512l254.72-254.72-45.226667-45.226667L512 466.773333 257.28 212.053333 212.053333 257.28 466.773333 512z" fill="#e1e1e1" p-id="1199"></path></svg>`)
-    tabWrap.insertBefore(filetab, tabWrap.lastElementChild!)
+      + (keepalive ? '' : `<svg t="1631378872341" class="icon-close" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1198" width="14" height="14"><path d="M466.773333 512l-254.72 254.72 45.226667 45.226667L512 557.226667l254.72 254.72 45.226667-45.226667L557.226667 512l254.72-254.72-45.226667-45.226667L512 466.773333 257.28 212.053333 212.053333 257.28 466.773333 512z" fill="#e1e1e1" p-id="1199"></path></svg>`)
+    tabWrap.insertBefore(filetab, tabWrap.lastElementChild!.previousElementSibling)
     return filetab
   }
 
   function removeMonacoEditorModel () {
     const activeEditor = editorManage.getActive()
     activeEditor.forEach(async editorState => {
-      await editorState.editor.removeModel()
+      editorState.editor.removeModel()
     })
   }
 
   function removeFile(e: MouseEvent) {
     removeMonacoEditorModel()
+    if (filetab.hasAttribute("active")) {
+      ;(filetab.previousElementSibling as HTMLButtonElement).click()
+    }
     filetab.remove()
-    ;(<HTMLButtonElement>tabWrap.children.item(tabWrap.children.length - 2)).click()
     e.stopPropagation()
   }
 
@@ -176,29 +247,58 @@ function setupIframesandbox (host: CodePlayground) {
   return sandbox
 }
 
-export default class CodePlayground extends HTMLElement {
-  private editors: Record<string, MonacoEditorItem> = {}
-  public fs = new FileSystem<CompiledFile>()
-  public editorManage
-  public project = createProjectManager("vue")
+function clickshowInput (e: MouseEvent) {
+  let target = e.currentTarget! as HTMLElement
+  const input = target.previousElementSibling as HTMLInputElement
+  input.classList.toggle("filename-input-show", true)
+  input.focus()
+}
 
-  constructor() {
-    super()
+function inputCreateFile (host: CodePlayground) {
+  return async function (e: KeyboardEvent) {
+    if (e.key === "Enter") {
+      const target = (e.target as HTMLInputElement)
+      const filename = target.value
+      if (isCreateAble(filename, host.fs)) {
+        await createFile(host, filename)
+        target.value = ""
+        target.classList.toggle("filename-input-show", false)
+      } else {
+        target.classList.toggle("filename-input-error", true)
+        setTimeout(() => {
+          target.classList.toggle("filename-input-error", false)
+        }, 400)
+      }
+    }
+  }
+}
+
+function fileInputBlur (e: FocusEvent) {
+  (e.target as HTMLElement).classList.toggle("filename-input-show", false)
+}
+
+export default class CodePlayground extends HTMLElement {
+  public fs = new FileSystem<CompiledFile>()
+  public project = createProjectManager("vue")
+  public editorManage!: ReturnType<typeof createMonacoEditorManager>
+  private createFileEvent = inputCreateFile(this)
+  async connectedCallback () {
     const wrap = this.ownerDocument.createElement("div")
     wrap.innerHTML = teamplateElement
     wrap.style.width = "inherit"
     wrap.style.height = "inherit"
     this.appendChild(wrap)
-    this.editorManage = this.createMonacoEditorManager()
-  }
+    this.editorManage = createMonacoEditorManager(this)
 
-  async connectedCallback () {
-    const project = await this.project
-
+    const { project, addButton, addInput } = this
+    const projectManage = await project
     const sandbox = setupIframesandbox(this)
-    sandbox.setupDependency(project.getRuntimeImportMap())
+    sandbox.setupDependency(projectManage.getRuntimeImportMap())
+    addButton.addEventListener("click", clickshowInput)
+    addInput.addEventListener("keydown", this.createFileEvent)
+    addInput.addEventListener("blur", fileInputBlur)
+
     await createFile(this, "app.vue", true)
-    await createFile(this, "app.ts")
   }
 
   disconnectedCallback () {
@@ -217,58 +317,12 @@ export default class CodePlayground extends HTMLElement {
     return this.ownerDocument.querySelector("#tab")!
   }
 
-  private createMonacoEditorManager () {
-    const { editors, editorWrap } = this
-    const manager = {
-      show: (type: SupportEditorType): MonacoEditorItem => {
-        let state = editors[type]
-        if (!state) {
-          const editor = new MonacoEditor()
-          editor.style.width = "100%"
-          editor.style.height = "100%"
+  get addButton (): HTMLButtonElement {
+    return this.ownerDocument.querySelector("#tab .icon-add")!
+  }
 
-          const wrap = this.ownerDocument.createElement("div")
-          wrap.style.width = "100%"
-          wrap.style.height = "100%"
-          wrap.setAttribute("slot", "item")
-          wrap.appendChild(editor)
-
-          state = { wrap, editor, status: false }
-          editors[type] = state
-          editorWrap.appendChild(state.wrap)
-        }
-        if (!state.status) {
-          state.status = true
-          state.wrap.style.display = "block"
-          state.wrap.removeAttribute("hidden")
-        }
-        return state
-      },
-
-      getActive: () => {
-        const result = []
-        for (let key in editors) {
-          const editor = editors[key]
-          if (editor.status) {
-            result.push(editor)
-          }
-        }
-        return result
-      },
-
-      hide: (type: SupportEditorType) => {
-        const state = editors[type]
-        if (state && state.status) {
-          state.status = false
-          state.wrap.style.display = "none"
-          state.wrap.setAttribute("hidden", "")
-        }
-      },
-
-      hideAll: () => {
-        Object.keys(editors).forEach(editorType => manager.hide(editorType as any))
-      },
-    }
-    return manager
+  get addInput (): HTMLButtonElement {
+    return this.ownerDocument.querySelector("#filename-input")!
   }
 }
+
